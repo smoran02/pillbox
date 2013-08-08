476bd08df1dca089103b167035a3232486921831
Spencer Moran <smoran02@gmail.com>
2013-08-07 21:10:57 -0700

    Add secure token and rspec

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,19 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-MedsApp::Application.config.secret_key_base = 'a96b9a3fa70fec8d856bd3476ece6516c867e796d55aaa878b8c69b519b279092e99a0188d52b6e0d69e41364df4ad44635388bb3b4e51303e9ae4267d2c965f'
+require 'securerandom'
+
+def secure_token
+	token_file = Rails.root.join('.secret')
+	if File.exist?(token_file)
+		# Use the existing token
+		File.read(token_file).chomp
+	else
+		# Generate a new token and store it in token_file
+		token = SecureRandom.hex(64)
+		File.write(token_file, token)
+		token
+	end
+end
+
+MedsApp::Application.config.secret_key_base = secure_token
