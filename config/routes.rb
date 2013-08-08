8158d29332270cd1ca9639464c9d075c18c156eb
Spencer Moran <smoran02@gmail.com>
2013-08-07 22:46:36 -0700

    Add user resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 MedsApp::Application.routes.draw do
   root 'static_pages#home'
+  resources :users
 
   match '/about', to: 'static_pages#about', via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
