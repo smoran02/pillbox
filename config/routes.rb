f7bf168f623db9939030863f66917dc63c5149ed
Spencer Moran <smoran02@gmail.com>
2013-08-08 00:55:10 -0700

    Add sessions controller and signin route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 MedsApp::Application.routes.draw do
   root 'static_pages#home'
   resources :users
+  resources :sessions, only: [:create, :destroy, :new]
 
+  match '/signup', to: 'users#new', via: 'get'
+  match '/signin', to: 'sessions#new', via: 'get'
+  match '/signout', to: 'sessions#destroy', via: 'delete'
   match '/about', to: 'static_pages#about', via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
