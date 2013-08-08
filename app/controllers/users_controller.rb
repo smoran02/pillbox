6d37b5218d04a9851ed734fccf67df00e32baaca
Spencer Moran <smoran02@gmail.com>
2013-08-08 01:41:24 -0700

    Finish sign in/sign out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,14 +9,16 @@ class UsersController < ApplicationController
   end
 
   def create
-  	@user = User.new(user_params)
-  	if @user.save
-  		flash[:success] = "Welcome to PillBox!"
-  		redirect_to @user
-  	else
-  		render 'new'
-  	end
-  end
+  	def create
+	    @user = User.new(user_params)
+	    if @user.save
+	      sign_in @user
+	      flash[:success] = "Welcome to the Sample App!"
+	      redirect_to @user
+	    else
+	      render 'new'
+	    end
+	  end
 
   private
 
