360dcee8deac2b2e286ef71e650a7d84002a3206
Spencer Moran <smoran02@gmail.com>
2013-08-07 21:03:13 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MedsApp::Application.config.secret_key_base = 'a96b9a3fa70fec8d856bd3476ece6516c867e796d55aaa878b8c69b519b279092e99a0188d52b6e0d69e41364df4ad44635388bb3b4e51303e9ae4267d2c965f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MedsApp::Application.config.session_store :cookie_store, key: '_meds_app_session'
