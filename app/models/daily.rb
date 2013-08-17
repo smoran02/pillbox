e2d006a2af3ed460e4314c9d099170dc474744b4
Spencer Moran <smoran02@gmail.com>
2013-08-16 21:40:04 -0700

    Add user/daily relationship, add daily validations

--- app/models/daily.rb
+++ app/models/daily.rb
@@ -1,3 +1,6 @@
 class Daily < ActiveRecord::Base
+	belongs_to :user
+	default_scope -> { order('created_at ASC') }
 	validates :user_id, presence: true
+	validates :report_for, uniqueness: true
 end
