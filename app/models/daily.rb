e8b94de171856cfc476429073f4054250ef26d32
Spencer Moran <smoran02@gmail.com>
2013-08-18 23:09:15 -0700

    Add report_for uniqueness validation scoped to user_id

--- app/models/daily.rb
+++ app/models/daily.rb
@@ -2,4 +2,5 @@ class Daily < ActiveRecord::Base
 	belongs_to :user
 	default_scope -> { order('created_at ASC') }
 	validates :user_id, presence: true
+	validates :report_for, uniqueness: { scope: :user_id }
 end
