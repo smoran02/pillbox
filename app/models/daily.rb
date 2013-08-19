a95450902d39d030e4db67287251f08457b59f8e
Spencer Moran <smoran02@gmail.com>
2013-08-19 12:27:43 -0700

    Refactor scoped uniqueness validation for dailies

--- app/models/daily.rb
+++ app/models/daily.rb
@@ -1,6 +1,5 @@
 class Daily < ActiveRecord::Base
 	belongs_to :user
 	default_scope -> { order('created_at ASC') }
-	validates :user_id, presence: true
-	validates :report_for, uniqueness: { scope: :user_id }
+	validates :user_id, uniqueness: { scope: :report_for }
 end
