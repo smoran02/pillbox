75f4961f32d26ad25c6c174303baacf1fb8c05b9
Spencer Moran <smoran02@gmail.com>
2013-08-26 12:51:33 -0700

    Weekly view finished except for BP helpers

--- app/helpers/dailies_helper.rb
+++ app/helpers/dailies_helper.rb
@@ -3,7 +3,24 @@ module DailiesHelper
 		if daily.nil?
 			""
 		else
-			daily[attribute]
+			if daily[attribute].nil?
+				""
+			else
+				daily[attribute]
+			end
 		end
 	end
+
+	def time_output(daily, attribute)
+		if daily.nil?
+			""
+		else
+			if daily[attribute].nil?
+				""
+			else
+				daily[attribute].strftime("%l:%M %p")
+			end
+		end
+  end
+
 end
