a0833ad775fcdd94f80608f267745e5e359d3dba
Spencer Moran <smoran02@gmail.com>
2013-08-28 17:12:49 -0700

    Add nap_output helper, refactor other daily helpers

--- app/helpers/dailies_helper.rb
+++ app/helpers/dailies_helper.rb
@@ -1,26 +1,28 @@
 module DailiesHelper
 	def format_output(daily, attribute)
-		if daily.nil?
+		if daily.nil? || daily[attribute].nil?
 			""
 		else
-			if daily[attribute].nil?
-				""
-			else
-				daily[attribute]
-			end
+			daily[attribute]
 		end
 	end
 
 	def time_output(daily, attribute)
-		if daily.nil?
+		if daily.nil? || daily[attribute].nil?
 			""
 		else
-			if daily[attribute].nil?
-				""
-			else
-				daily[attribute].strftime("%l:%M %p")
-			end
+			daily[attribute].strftime("%l:%M %p")
 		end
+  end
+
+  def nap_output(daily)
+  	if daily.nil? || daily[:nap].nil?
+  		""
+  	elsif daily[:nap] == true
+  		"Yes"
+  	else
+  		"No"
+  	end
   end
 
 end
